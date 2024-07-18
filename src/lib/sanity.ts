5ada10935d0e3d4eed9dae29f8968d13f23478c4
McFly <matthieu.roman2002@gmail.com>
2024-07-18 02:16:22 +0200

    push

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -2,14 +2,14 @@ import { createClient } from "@sanity/client"
 import imageUrlBuilder from "@sanity/image-url"
 
 export const client = createClient({
-	apiVersion: "2022-03-07",
-	dataset: "production",
-	projectId: "tz7lah1m",
-	useCdn: false,
+  apiVersion: "2022-03-07",
+  dataset: "production",
+  projectId: "tz7lah1m",
+  useCdn: true,
 })
 
 const builder = imageUrlBuilder(client)
 
 export function urlFor(source: any) {
-	return builder.image(source)
+  return builder.image(source)
 }
